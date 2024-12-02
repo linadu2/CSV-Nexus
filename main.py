9e53d116397be410badaaef0563fdd601c9cf8d9
linadu2 <pierrealex1150@gmail.com>
2024-12-02 15:32:53 +0100

    add the main function

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import csv
 import os
+import argparse as ag
+
+from numpy.ma.extras import row_stack
+
 
 def equality_check(arr1: list[str], arr2: list[str]) -> bool:
    if (len(arr1) != len(arr2)):
       return False
-   arr1.sort()
-   arr2.sort()
    for i in range(0, len(arr2)):
       if arr1[i] != arr2[i]:
          return False
@@ -13,36 +15,81 @@ def equality_check(arr1: list[str], arr2: list[str]) -> bool:
 
 
 
-def load_csv(file: str, delimiter: str='s') -> tuple[list[str], list[list[str]]]:
-    if f"{file}.csv" not in os.listdir('.'):
-        raise AttributeError("csv file not in the directory")
-    with open(f'{file}.csv', newline='', encoding='utf-8') as csvfile:
+def load_csv(file: str, delimiter: str=',') -> tuple[list[str], list[list[str]]]:
+    if file not in os.listdir('.'):
+        raise FileNotFoundError("csv file not in the directory")
+    with open(file, newline='', encoding='utf-8') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=delimiter, quotechar='|')
-        data = []
+        temp = []
         for row in spamreader:
-            data.append(row)
+            temp.append(row)
+        header = temp[:1][0]
+        header.append('region')
+        temp = temp[1:]
+        data = []
+        data += [[row[0], float(row[1]), float(row[2]), row[3], os.path.splitext(file)[0]] for row in temp]
 
-        return data[:1][0], data[1:]
+        #print(header)
+        return header, data
 
 
 def write_csv(file: str, data: list[list[str]], header:list[str], delimiter: str=',') -> None:
-    print(f'{file}.csv' in os.listdir('.'))
-    if f'{file}.csv' in os.listdir('.'):
+    print(file in os.listdir('.'))
+    if file in os.listdir('.'):
         choix = None
         while choix != 'yes' and choix != 'n':
             choix = input('warning file exist, would you overwrite it ? (yes/n)')
-            print(choix)
         if choix == 'n':
             return
 
-    with open(f'{file}.csv', 'w', newline='', encoding='utf-8') as csvfile:
+    with open(file, 'w', newline='', encoding='utf-8') as csvfile:
         csv_writer = csv.writer(csvfile, delimiter=delimiter)
         csv_writer.writerow(header)
         csv_writer.writerows(data)
 
 
 def main():
-    pass
+    parser = ag.ArgumentParser(description='A programme to performe various operation on csv file')
+    parser.add_argument('merge', nargs='+')
+    parser.add_argument('-o', '--output', default=None, nargs='?')
+    parser.add_argument('-s', '--sort', default=None, nargs='?')
+
+    args = parser.parse_args()
+
+    #print(args.merge)
+    #print(args.output)
+    #print(args.sort)
+    if args.output.endswith('.csv'):
+        output_file = args.output
+    else:
+        raise ValueError("the output file is not a csv file")
+
+    data = []
+    header = None
+    for x in args.merge:
+        h, d = load_csv(x)
+        if not header:
+            data += d
+            header = h
+        else:
+            if equality_check(h, header):
+                data += d
+            else:
+                raise AttributeError('header does not match between csv file')
+    #print(header)
+    #[print(row) for row in data]
+
+    #print(header, args.sort )
+    if args.sort in header:
+        print(header.index(args.sort))
+        data = sorted(data, key=lambda x: x[header.index(args.sort)])
+        [print(row) for row in data]
+    else:
+        raise ValueError('the sort parameter don\'t match a colone')
+
+    if output_file:
+        write_csv(output_file, data, header)
+
 
 
 
