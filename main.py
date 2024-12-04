53a956d877120b6d1d3a171fdd7bb83f71992ccb
linadu2 <pierrealex1150@gmail.com>
2024-12-04 14:30:36 +0100

    .

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import argparse as ag
 
 
 def equality_check(arr1: list[str], arr2: list[str]) -> bool:
-   if (len(arr1) != len(arr2)):
+   if len(arr1) != len(arr2):
       return False
    for i in range(0, len(arr2)):
       if arr1[i] != arr2[i]:
@@ -32,14 +32,15 @@ def load_csv(file: str, delimiter: str=',') -> tuple[list[str], list[list[str]]]
         return header, data
 
 
-def write_csv(file: str, data: list[list[str]], header:list[str], delimiter: str=',') -> None:
-    print(file in os.listdir('.'))
-    if file in os.listdir('.'):
-        choix = None
-        while choix != 'yes' and choix != 'n':
-            choix = input('warning file exist, would you overwrite it ? (yes/n)')
-        if choix == 'n':
-            return
+def write_csv(file: str, data: list[list[str]], header:list[str], delimiter: str=',', force_overwrite: bool= False) -> None:
+    #print(file in os.listdir('.'))
+    if not force_overwrite:
+        if file in os.listdir('.'):
+            choix = None
+            while choix != 'yes' and choix != 'n':
+                choix = input('warning file exist, would you overwrite it ? (yes/n)')
+            if choix == 'n':
+                return
 
     with open(file, 'w', newline='', encoding='utf-8') as csvfile:
         csv_writer = csv.writer(csvfile, delimiter=delimiter)
@@ -47,25 +48,8 @@ def write_csv(file: str, data: list[list[str]], header:list[str], delimiter: str
         csv_writer.writerows(data)
 
 
-def main():
-    parser = ag.ArgumentParser(description='A programme to performe various operation on csv file')
-    parser.add_argument('file', nargs='+')
-    parser.add_argument('-o', '--output', default=None, nargs='?')
-    parser.add_argument('-s', '--sort', default=None, nargs='?')
-
-    args = parser.parse_args()
-
-    #print(args.merge)
-    #print(args.output)
-    #print(args.sort)
-    if args.output.endswith('.csv'):
-        output_file = args.output
-    else:
-        raise ValueError("the output file is not a csv file")
-
-    data = []
-    header = None
-    for x in args.merge:
+def merge_csv(file, data, header):
+    for x in file:
         h, d = load_csv(x)
         if not header:
             data += d
@@ -75,19 +59,57 @@ def main():
                 data += d
             else:
                 raise AttributeError('header does not match between csv file')
-    #print(header)
-    #[print(row) for row in data]
+    return header, data
 
-    #print(header, args.sort )
-    if args.sort in header:
-        print(header.index(args.sort))
-        data = sorted(data, key=lambda x: x[header.index(args.sort)])
+
+def sort_data(data, header, column):
+    if column in header:
+        print(header.index(column))
+        data = sorted(data, key=lambda x: x[header.index(column)])
         [print(row) for row in data]
     else:
         raise ValueError('the sort parameter don\'t match a colone')
 
-    if output_file:
-        write_csv(output_file, data, header)
+    return data
+
+
+def main():
+    parser = ag.ArgumentParser(description='A programme to performe various operation on csv file', prog='CSV-Nexus')
+    parser.add_argument('file', nargs='+', help='the file to load in the programme, if multiple file is given it will be merge')
+    parser.add_argument('-o', '--output', default=None, nargs='?', help='a output file if you want to save the result in a csv file')
+    parser.add_argument('-s', '--sort', default=None, nargs='?', help='a column to sort the data')
+    parser.add_argument('--force-overwrite', action='store_true', help='to force the overwrite if the output file exist')
+
+    args = parser.parse_args()
+
+    #print(args.merge)
+    #print(args.output)
+    #print(args.sort)
+    #print(args.force_overwrite)
+    if args.output:
+        if not args.output.endswith('.csv'):
+            raise ValueError("the output file is not a csv file")
+
+    data = []
+    header = None
+    if len(args.file) > 1:
+        header, data = merge_csv(args.file, data, header)
+    else:
+        header, data = load_csv(args.file[0])
+
+    #print(header)
+    #[print(row) for row in data]
+
+    #print(header, args.sort )
+
+    if args.sort:
+        data = sort_data(data, header, args.sort)
+
+    if args.output:
+        write_csv(args.output, data, header, force_overwrite = args.force_overwrite)
+    else:
+        print(header)
+        [print(row) for row in data]
 
 
 
