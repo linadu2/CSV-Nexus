1e08a7a7611b5fb9f45058f0738e0ab09f39f379
linadu2 <pierrealex1150@gmail.com>
2024-12-06 10:51:33 +0100

    copy lib to v1

---
+++ V1/lib.py
@@ -0,0 +1,72 @@
+import os
+import csv
+
+
+def equality_check(arr1: list[str], arr2: list[str]) -> bool:
+   if len(arr1) != len(arr2):
+      return False
+   for i in range(0, len(arr2)):
+      if arr1[i] != arr2[i]:
+         return False
+   return True
+
+
+
+def load_csv(file: str, delimiter: str=',') -> tuple[list[str], list[list[str]]]:
+    if file not in os.listdir('.'):
+        raise FileNotFoundError("csv file not in the directory")
+    with open(file, newline='', encoding='utf-8') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=delimiter, quotechar='|')
+        temp = []
+        for row in spamreader:
+            temp.append(row)
+        header = temp[:1][0]
+        header.append('département')
+        temp = temp[1:]
+        data = []
+        data += [[row[0], float(row[1]), float(row[2]), row[3], os.path.splitext(file)[0]] for row in temp]
+
+        #print(header)
+        return header, data
+
+
+def write_csv(file: str, data: list[list[str]], header:list[str], delimiter: str=',', force_overwrite: bool= False) -> None:
+    #print(file in os.listdir('.'))
+    if not force_overwrite:
+        if file in os.listdir('.'):
+            choix = None
+            while choix != 'yes' and choix != 'n':
+                choix = input('warning file exist, would you overwrite it ? (yes/n)')
+            if choix == 'n':
+                return
+
+    with open(file, 'w', newline='', encoding='utf-8') as csvfile:
+        csv_writer = csv.writer(csvfile, delimiter=delimiter)
+        csv_writer.writerow(header)
+        csv_writer.writerows(data)
+
+
+def merge_csv(file, data, header):
+    for x in file:
+        h, d = load_csv(x)
+        if not header:
+            data += d
+            header = h
+        else:
+            if equality_check(h, header):
+                data += d
+            else:
+                raise AttributeError('header does not match between csv file')
+    return header, data
+
+
+
+def sort_data(data, header, column, reverse: bool = False):
+    if column in header:
+        #print(header.index(column))
+        data = sorted(data, key=lambda x: x[header.index(column)], reverse=reverse)
+        #[print(row) for row in data]
+    else:
+        raise ValueError('the sort parameter don\'t match a column')
+
+    return data
