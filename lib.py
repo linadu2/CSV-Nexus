f6083fcad0aea157880c00b70386d0ecdd6845ee
linadu2 <pierrealex1150@gmail.com>
2024-12-09 16:43:58 +0100

    handling of bad formated file

--- lib.py
+++ lib.py
@@ -20,11 +20,22 @@ def load_csv(file: str, delimiter: str=',') -> tuple[list[str], list[list[str]]]
         temp = []
         for row in spamreader:
             temp.append(row)
+        if not temp:
+            raise ValueError("csv file empty")
+        if len(temp[0]) != 4:
+            raise ValueError("csv file must contain exactly 4 columns")
+        if len(temp) < 2:
+            raise ValueError("csv file must contain at least 1 header and 1 line of data")
         header = temp[:1][0]
         header.append('département')
         temp = temp[1:]
         data = []
-        data += [[row[0], float(row[1]), float(row[2]), row[3], os.path.splitext(file)[0]] for row in temp]
+        try:
+            data += [[row[0], float(row[1]), float(row[2]), row[3], os.path.splitext(file)[0]] for row in temp]
+        except ValueError:
+            raise ValueError("Row contains invalid data types (expected str, float, float, str)")
+        except IndexError:
+            raise IndexError("Not enough rows in csv file")
 
         #print(header)
         return header, data
